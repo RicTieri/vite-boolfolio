18536dd4f8a5abf9557d7b2ab5359773b200be49
RicTieri <tieri.riccardo@gmail.com>
2024-02-26 21:31:33 +0100

    added console.log response

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -14,7 +14,7 @@ export default {
       })
         .then(function (response) {
           // handle success
-          console.log(response);
+          console.log(response.data);
         })
         .catch(function (error) {
           // handle error
